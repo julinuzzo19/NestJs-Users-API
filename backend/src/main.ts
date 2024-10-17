793caec4b74bea2caa3584b9b98fdb1e58e44748
julinuzzo19 <julinuzzo19@gmail.com>
2024-10-17 00:30:33 -0300

    added cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
-  app.enableCors();
+  app.enableCors({ methods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE'] });
   app.useLogger(WinstonModule.createLogger({ instance: logger }));
   await app.listen(3010);
 }
